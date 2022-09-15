9b2124a4f4ed71bb33bb5af4af86763c967f6a98
kezhengjie <kzj961014@qq.com>
2022-09-15 14:24:28 +0800

    trivial_update

--- tests/test_dns_query.cpp
+++ tests/test_dns_query.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <thread>
 #include "dns_query/dns_query.h"
 
 int main(int argc, char** argv)
@@ -18,55 +17,5 @@ int main(int argc, char** argv)
                                std::cout << v << std::endl;
                            }
                        });
-    std::thread t(
-        [&]() -> void
-        {
-            query.Run(false);
-        });
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    query.AsyncResolve("www.baidu.com", [&](const dns::Result& result) -> void
-                       {
-                           if (result.HasError())
-                           {
-                               std::cout << result.Error() << std::endl;
-                               return;
-                           }
-                           std::cout << result.Name() << std::endl;
-                           for (const auto& v : result)
-                           {
-                               std::cout << v << std::endl;
-                           }
-                       });
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    query.AsyncResolve("www.baidu.com", [&](const dns::Result& result) -> void
-                       {
-                           if (result.HasError())
-                           {
-                               std::cout << result.Error() << std::endl;
-                               return;
-                           }
-                           std::cout << result.Name() << std::endl;
-                           for (const auto& v : result)
-                           {
-                               std::cout << v << std::endl;
-                           }
-                       });
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    query.AsyncResolve("www.baidu.com", [&](const dns::Result& result) -> void
-                       {
-                           if (result.HasError())
-                           {
-                               std::cout << result.Error() << std::endl;
-                               return;
-                           }
-                           std::cout << result.Name() << std::endl;
-                           for (const auto& v : result)
-                           {
-                               std::cout << v << std::endl;
-                           }
-                       });
-
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    query.Stop();
-    t.join();
+    query.Run();
 }

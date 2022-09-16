f83cecae46d49e9fc7995a868e7bd05399b1686c
kezhengjie <kzj961014@qq.com>
2022-09-16 13:09:02 +0800

    trivial_update

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # dns_query
-C++ DNS query lib,based on c-ares.Provide simple API.
+C++ async dns resolve library.Provide asio-callback-like API.
+
+
+## Quick Start
+```c++
+#include <iostream>
+#include "dns_query/dns_query.h"
+
+int main(int argc, char** argv)
+{
+    dns::DNSQuery query;
+    query.AsyncResolve("www.google.com", [&](const dns::Result& result) -> void
+                       {
+                           if (result.HasError())
+                           {
+                               std::cout << result.Error() << std::endl;
+                           }
+                           else
+                           {
+                               std::cout << result.Name() << std::endl;
+                               for (const auto& v : result)
+                               {
+                                   std::cout << v << std::endl;
+                               }
+                           }
+                       });
+    query.Run(true);
+}
+
+```
+
+## Introduction
+* High performance based on c-ares,a C language dns async resolve library.
+* Provide C++ style and simple API,also runtime safe.
+
+
+## Usage
+
+1. Init submodule,run  
+   ```bash
+   git submodule update --init
+   ```
+2. Use CMake to build library.
+3. Include to your project.
+for CMake user,use
+```cmake
+add_subdirectory(/path/to/dns_query)
+
+target_include_directories(your_project
+    PRIVATE
+        /path/to/dns_query/include
+)
+
+target_link_libraries(your_project
+    PRIVATE
+        ...
+        dns_query::lib
+    )
+```
+
+
+
+## Notice
+
+* It is currently thread not safe,we recommand you to instantiate `dns::DNSQuery` once and  
+call `dns::DNSQuery::AsyncResolve` in one thread or use mutex to keep sync.  
--- tests/test_dns_query.cpp
+++ tests/test_dns_query.cpp
@@ -1,44 +1,42 @@
 #include <iostream>
-#include <thread>
 #include "dns_query/dns_query.h"
 
 int main(int argc, char** argv)
 {
     dns::DNSQuery query;
-    query.AsyncResolve("www.baidu.com", [&](const dns::Result& result) -> void
+    query.AsyncResolve("www.google.com", [&](const dns::Result& result) -> void
                        {
                            if (result.HasError())
                            {
                                std::cout << result.Error() << std::endl;
-                               return;
                            }
-                           std::cout << result.Name() << std::endl;
-                           for (const auto& v : result)
+                           else
                            {
-                               std::cout << v << std::endl;
+                               std::cout << result.Name() << std::endl;
+                               for (const auto& v : result)
+                               {
+                                   std::cout << v << std::endl;
+                               }
                            }
+                           query.AsyncResolve("www.youtube.com", [&](const dns::Result& result) -> void
+                                              {
+                                                  if (result.HasError())
+                                                  {
+                                                      std::cout << result.Error() << std::endl;
+                                                  }
+                                                  else
+                                                  {
+                                                      std::cout << result.Name() << std::endl;
+                                                      for (const auto& v : result)
+                                                      {
+                                                          std::cout << v << std::endl;
+                                                      }
+                                                  }
+                                              });
                        });
-    std::thread t(
-        [&]() -> void
-        {
-            query.Run(false);
-        });
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::cout << "next" << std::endl;
-    query.AsyncResolve("www.baidu.com", [&](const dns::Result& result) -> void
-                       {
-                           if (result.HasError())
-                           {
-                               std::cout << result.Error() << std::endl;
-                               return;
-                           }
-                           std::cout << result.Name() << std::endl;
-                           for (const auto& v : result)
-                           {
-                               std::cout << v << std::endl;
-                           }
-                       });
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    query.Stop();
-    t.join();
+    // Error DNSQuery::Run(bool stop_at_end)
+    // Run will return if all async task is finished;
+    // pass false to Run() if you need it to keep running and resolve
+    // more dns in the future.
+    query.Run(true);
 }
